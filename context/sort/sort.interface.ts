e5ba364ae930b40a88dc3adaff0ba11245258fea
itsmichaelbtw <itsmichaelbtw@gmail.com>
2024-04-11 17:22:23 +1000

    feat: sort context with ref injectors

---
+++ context/sort/sort.interface.ts
@@ -0,0 +1,42 @@
+import type { ContextReducerInterface, InitialStateWithDispatch } from "context/context.types";
+import type { Algorithm, ViewModes, Speed } from "types";
+
+export type SortReducerActionType =
+  | "SET_VIEW_MODE"
+  | "SET_ALGORITHM"
+  | "SET_SPEED"
+  | "SET_COLUMN_COUNT"
+  | "SET_SORTING"
+  | "SET_PAUSED";
+
+export type SortAction = ContextReducerInterface<SortReducerActionType, SortContextInterface>;
+
+type SortingStatus = "idle" | "sorting" | "pause" | "done";
+
+interface SortInjector {
+  status: SortingStatus[];
+  updateSortStatus(index: number, status: SortingStatus): void;
+  stopSort(index: number): boolean;
+}
+
+export interface SortContextInterface {
+  viewMode: ViewModes;
+  algorithm: Algorithm;
+  speed: Speed;
+  columnCount: number;
+  isPaused: boolean;
+  isSorting: boolean;
+  sortInjector: SortInjector;
+  changeViewMode(viewMode: ViewModes): void;
+  changeAlgorithm(algorithm: Algorithm): void;
+  adjustSpeed(speed: Speed): void;
+  updateColumnCount(columnCount: number): void;
+  dispatchSort(value: boolean): void;
+  dispatchPause(): void;
+}
+
+export type InitialSortContext = InitialStateWithDispatch<
+  SortReducerActionType,
+  SortContextInterface
+> &
+  SortContextInterface;
---
+++ context/sort/sort.reducer.ts
@@ -0,0 +1,30 @@
+import type { SortAction, SortContextInterface } from "./sort.interface";
+
+import { withDispatchReducer } from "context/context.dispatch";
+
+export function sortReducer(state: SortContextInterface, action: SortAction): SortContextInterface {
+  const dispatch = withDispatchReducer(state, action);
+
+  return dispatch(() => {
+    switch (action.type) {
+      case "SET_ALGORITHM":
+        return { ...state, algorithm: action.payload.algorithm! };
+      case "SET_SPEED":
+        return { ...state, speed: action.payload.speed! };
+      case "SET_VIEW_MODE":
+        return { ...state, viewMode: action.payload.viewMode! };
+
+      case "SET_COLUMN_COUNT":
+        return { ...state, columnCount: action.payload.columnCount! };
+
+      case "SET_SORTING":
+        return { ...state, isSorting: action.payload.isSorting!, isPaused: false };
+
+      case "SET_PAUSED":
+        return { ...state, isPaused: true, isSorting: false };
+
+      default:
+        return state;
+    }
+  });
+}
