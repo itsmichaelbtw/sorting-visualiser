201b20b9917c574a54fde309dc225bc31b3b5df3
itsmichaelbtw <itsmichaelbtw@gmail.com>
2024-04-14 21:17:17 +1000

    chore: minor changes to app root files

--- index.tsx
+++ index.tsx
@@ -1,19 +1,13 @@
 import type { WithChildren } from "./types";
 
 import "./css/tailwind.css";
-import "./css/root.css";
 
 import React from "react";
 import ReactDom from "react-dom/client";
 
 import { App } from "components/App";
-import { DevelopmentEnvironment } from "components/containers/developer-menu/DevelopmentEnvironment";
-
-const DeveloperMenu = React.lazy(() =>
-  import("components/containers/developer-menu/DeveloperMenu").then(({ DeveloperMenu }) => ({
-    default: DeveloperMenu
-  }))
-);
+import { RestrictEnvironment } from "components/elements/RestrictEnvironment";
+import { LazyComponent } from "components/elements/LazyComponent";
 
 const container = document.querySelector("#root");
 const root = ReactDom.createRoot(container!);
@@ -29,8 +23,13 @@ function WithStrictMode({ children }: WithChildren) {
 root.render(
   <WithStrictMode>
     <App />
-    <DevelopmentEnvironment>
-      <DeveloperMenu />
-    </DevelopmentEnvironment>
+    <RestrictEnvironment environment="development">
+      <LazyComponent
+        objectName="DeveloperMenu"
+        __import={() => {
+          return import("components/elements/DeveloperMenu");
+        }}
+      />
+    </RestrictEnvironment>
   </WithStrictMode>
 );
